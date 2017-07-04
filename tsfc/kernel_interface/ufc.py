200db948e5a7747f5ecc3698241e7ce03d5ba563
S Nicholas Barton <nicholasbarton@Ss-MacBook-Pro.local>
2017-07-04 13:53:10 +0100

    update kernel interfaces to use new scalar type

--- tsfc/kernel_interface/ufc.py
+++ tsfc/kernel_interface/ufc.py
@@ -15,7 +15,7 @@ from finat import TensorFiniteElement
 
 from tsfc.kernel_interface.common import KernelBuilderBase
 from tsfc.finatinterface import create_element
-from tsfc.coffee import SCALAR_TYPE
+from tsfc.parameters import scalar_type
 
 
 class KernelBuilder(KernelBuilderBase):
@@ -77,7 +77,7 @@ class KernelBuilder(KernelBuilderBase):
         """
         name = "w"
         self.coefficient_args = [
-            coffee.Decl(SCALAR_TYPE, coffee.Symbol(name),
+            coffee.Decl(scalar_type(), coffee.Symbol(name),
                         pointers=[("const",), ()],
                         qualifiers=["const"])
         ]
@@ -234,16 +234,16 @@ def prepare_coordinates(coefficient, name, interior_facet=False):
     size = numpy.prod(shape, dtype=int)
 
     if not interior_facet:
-        funargs = [coffee.Decl(SCALAR_TYPE, coffee.Symbol(name),
+        funargs = [coffee.Decl(scalar_type(), coffee.Symbol(name),
                                pointers=[("",)],
                                qualifiers=["const"])]
         variable = gem.Variable(name, (size,))
         expression = gem.reshape(variable, shape)
     else:
-        funargs = [coffee.Decl(SCALAR_TYPE, coffee.Symbol(name+"_0"),
+        funargs = [coffee.Decl(scalar_type(), coffee.Symbol(name+"_0"),
                                pointers=[("",)],
                                qualifiers=["const"]),
-                   coffee.Decl(SCALAR_TYPE, coffee.Symbol(name+"_1"),
+                   coffee.Decl(scalar_type(), coffee.Symbol(name+"_1"),
                                pointers=[("",)],
                                qualifiers=["const"])]
         variable0 = gem.Variable(name+"_0", (size,))
@@ -269,7 +269,7 @@ def prepare_arguments(arguments, multiindices, interior_facet=False):
          expressions - GEM expressions referring to the argument
                        tensor
     """
-    funarg = coffee.Decl(SCALAR_TYPE, coffee.Symbol("A"), pointers=[()])
+    funarg = coffee.Decl(scalar_type(), coffee.Symbol("A"), pointers=[()])
     varexp = gem.Variable("A", (None,))
 
     if len(arguments) == 0:
